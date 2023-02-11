b1c776ea0b015aac327e54589efa12a5b677a588
Jorge Neiva <jorge_neiva_@hotmail.com>
2023-02-11 12:48:04 +0000

    list style changes and style buttons

--- restaurant/src/components/Navbar/Navbar.jsx
+++ restaurant/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import './Navbar.css'
 const MainBar = () => {
     return(
         <nav className="nav-bar">
-            <input className="search-bar" type="text" placeholder="Search..." />
             <input className="my-home" type="submit" value='Home' />
+            <input className="search-bar" type="text" placeholder="Search..." />
             <input className="contacts" type="submit" value= "Where are we" />
             <input className="cart" type="submit" value="Order" />
         </nav>
--- restaurant/src/components/productList/ProductList.jsx
+++ restaurant/src/components/productList/ProductList.jsx
@@ -1,13 +1,42 @@
 
 import './ProductList.css'
 import Picanha from '../../assets/images/picanha.png'
+import Pizza from '../../assets/images/Pizza.png'
 
 const FoodList = () => {
     return(
         <ul  className = 'meal-main'>
                 <li className='meal-box' >
-                    <div className='meal'>
-                        <div>
+                    <div >
+                        <div className='meal'>
+                            <img className='meal-img' src = {Picanha} alt="Prato em cima da mesa" />
+                        </div>
+                         <div>
+                             <h1>Picanha</h1>
+                             <h2>Deliciosa Picanha Argentina</h2>
+                             <p>15.90€</p>
+                             <input className="more-info" type="submit" value="Detalhe" />
+                             <input className="add-cart" type="submit" value="Encomendar" />
+                         </div>
+                    </div>
+                </li>
+                <li className='meal-box' >
+                    <div >
+                        <div className='meal'>
+                            <img src = {Pizza} alt="Prato em cima da mesa" />
+                        </div>
+                         <div>
+                             <h1>Pizza</h1>
+                             <h2>A nossa Pizza no forno</h2>
+                             <p>12.90€</p>
+                             <input className="more-info" type="submit" value="Detalhe" />
+                             <input className="add-cart" type="submit" value="Encomendar" />
+                         </div>
+                    </div>
+                </li>
+                <li className='meal-box' >
+                    <div >
+                        <div className='meal'>
                             <img src = {Picanha} alt="Prato em cima da mesa" />
                         </div>
                          <div>
@@ -20,8 +49,22 @@ const FoodList = () => {
                     </div>
                 </li>
                 <li className='meal-box' >
-                    <div className='meal'>
-                        <div className='meal-img'>
+                    <div >
+                        <div className='meal'>
+                            <img src = {Pizza} alt="Prato em cima da mesa" />
+                        </div>
+                         <div>
+                             <h1>Pizza</h1>
+                             <h2>A nossa Pizza no forno</h2>
+                             <p>12.90€</p>
+                             <input className="more-info" type="submit" value="Detalhe" />
+                             <input className="add-cart" type="submit" value="Encomendar" />
+                         </div>
+                    </div>
+                </li>
+                <li className='meal-box' >
+                    <div >
+                        <div className='meal'>
                             <img src = {Picanha} alt="Prato em cima da mesa" />
                         </div>
                          <div>
@@ -34,42 +77,14 @@ const FoodList = () => {
                     </div>
                 </li>
                 <li className='meal-box' >
-                    <div className='meal'>
-                        <div>
-                            <img src = {Picanha} alt="Prato em cima da mesa" />
+                    <div >
+                        <div className='meal'>
+                            <img src = {Pizza} alt="Prato em cima da mesa" />
                         </div>
                          <div>
-                             <h1>Picanha</h1>
-                             <h2>Deliciosa Picanha Argentina</h2>
-                             <p>15.90€</p>
-                             <input className="more-info" type="submit" value="Detalhe" />
-                             <input className="add-cart" type="submit" value="Encomendar" />
-                         </div>
-                    </div>
-                </li>
-                <li className='meal-box' >
-                    <div className='meal'>
-                        <div>
-                            <img src = {Picanha} alt="Prato em cima da mesa" />
-                        </div>
-                         <div>
-                             <h1>Picanha</h1>
-                             <h2>Deliciosa Picanha Argentina</h2>
-                             <p>15.90€</p>
-                             <input className="more-info" type="submit" value="Detalhe" />
-                             <input className="add-cart" type="submit" value="Encomendar" />
-                         </div>
-                    </div>
-                </li>
-                <li className='meal-box' >
-                    <div className='meal'>
-                        <div>
-                            <img src = {Picanha} alt="Prato em cima da mesa" />
-                        </div>
-                         <div>
-                             <h1>Picanha</h1>
-                             <h2>Deliciosa Picanha Argentina</h2>
-                             <p>15.90€</p>
+                             <h1>Pizza</h1>
+                             <h2>A nossa Pizza no forno</h2>
+                             <p>12.90€</p>
                              <input className="more-info" type="submit" value="Detalhe" />
                              <input className="add-cart" type="submit" value="Encomendar" />
                          </div>
